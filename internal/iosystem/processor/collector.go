8ecf69aadd20d88322795bd789769c6977a69b24
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2026-01-03 18:04:47 +0200

    update license

--- internal/iosystem/processor/collector.go
+++ internal/iosystem/processor/collector.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2025 Dmitry Kolesnikov
+// Copyright (C) 2025 - 2026 Dmitry Kolesnikov
 //
 // This file may be modified and distributed under the terms
 // of the MIT license.  See the LICENSE file for details.
